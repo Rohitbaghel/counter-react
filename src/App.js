60f2449ce38007fa1632e839eb5dbbc7175d65cd
Rohit Baghel <87422193+Rohitbaghel@users.noreply.github.com>
2021-12-24 17:40:14 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
 
 function App() {
+  const [count,Setcount] = useState(0)
+  function changeValue(value) {
+
+  Setcount(count+value)
+
+  }
+  function MultiplyBy2() {
+
+    Setcount(count*2)
+  
+    }
+ if( count > 20 ){
+return <div>Count is greater than</div>
+ }
+ if( count < 0 ){
+  return <div>Count can't be negative</div>
+   }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h3>changeValue:{count}</h3>
+      <button  onClick={()=>changeValue(1)}>add 1</button>
+    <button onClick={()=>changeValue(-1)}>delete 1</button>
+    <button onDoubleClick={MultiplyBy2}>Double Click</button>
+    <div><h1>count is {count%2==1 ? "Even" : "Odd"}</h1></div>
     </div>
   );
 }
